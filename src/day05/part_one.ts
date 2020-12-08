fe5074aaec0743bf69c7ec17a4f478c8880a74d9
John Roberts <john.calvin.roberts@gmail.com>
2020-12-09 01:53:22 +0800

    refactor day05

--- src/day05/part_one.ts
+++ src/day05/part_one.ts
@@ -25,33 +25,46 @@ import { readFile } from '../utils';
   The input is a list of boarding passes, find> what is the highest seat ID on a boarding pass?
  */
 
-const ROWS = 127;
-const COLUMNS = 7;
+export enum BoardingPassEncodings {
+  front = 'F',
+  back = 'B',
+  left = 'L',
+  right = 'R',
+}
 
-const mappings: Record<string, string[]> = {
-  rows: ['F', 'B'],
-  columns: ['L', 'R'],
-};
-
-const search = (string: string, length: number, map: string[]): number => {
-  let stack = Array.from({ length }, (_, i) => i);
-
-  for (let index = 0; index < string.length; index++) {
-    const letter = string[index];
-    const halfIndex = Math.ceil(stack.length / 2);
-    const firstHalf = stack.slice(0, halfIndex);
-    const secondHalf = stack.slice(halfIndex, stack.length);
-    const useFirst = map.indexOf(letter) === 0;
-    stack = useFirst ? firstHalf : secondHalf;
-  }
-  return stack[0];
-};
+export const lowerHalf = (l: number, u: number): [number, number] => [
+  l,
+  u - (u - l + 1) / 2,
+];
+export const upperHalf = (l: number, u: number): [number, number] => [
+  l + (u - l + 1) / 2,
+  u,
+];
 
 export const getSeatId = (boardingPass: string): number => {
-  const rows = boardingPass.slice(0, 7);
-  const columns = boardingPass.slice(7);
-  const row = search(rows, ROWS, mappings.rows);
-  const column = search(columns, COLUMNS, mappings.columns);
+  let rowsMax = 127;
+  let row = 0;
+  let columnsMax = 7;
+  let column = 0;
+  for (let index = 0; index < boardingPass.length; index++) {
+    const letter = boardingPass[index];
+    switch (letter) {
+      case BoardingPassEncodings.front:
+        [row, rowsMax] = lowerHalf(row, rowsMax);
+        break;
+      case BoardingPassEncodings.back:
+        [row, rowsMax] = upperHalf(row, rowsMax);
+        break;
+      case BoardingPassEncodings.left:
+        [column, columnsMax] = lowerHalf(column, columnsMax);
+        break;
+      case BoardingPassEncodings.right:
+        [column, columnsMax] = upperHalf(column, columnsMax);
+        break;
+      default:
+        break;
+    }
+  }
   return row * 8 + column;
 };
 
