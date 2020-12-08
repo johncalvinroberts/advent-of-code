b4063e445929e20a1cb693e952b968e4bc0e1103
John Roberts <john.calvin.roberts@gmail.com>
2020-12-09 01:53:42 +0800

    completes day05 part two

--- src/day05/part_two.ts
+++ src/day05/part_two.ts
@@ -1,8 +1,21 @@
 import path from 'path';
-import { readFile } from '@utils';
+import { readFile } from '../utils';
+import { getSeatId } from './part_one';
 
 export const runner = (input: string): number => {
-  return 0;
+  const boardingPasses: string[] = input.split('\n');
+  const ids = boardingPasses.map((item) => getSeatId(item));
+  const unclaimedSeats = Array.from(
+    { length: ids.length },
+    (_, i) => i + 1,
+  ).filter(
+    (seatNumber) =>
+      ids.indexOf(seatNumber) === -1 &&
+      ids.indexOf(seatNumber + 1) > -1 &&
+      ids.indexOf(seatNumber - 1) > -1,
+  );
+  console.log(unclaimedSeats);
+  return unclaimedSeats[0];
 };
 
 if (require.main === module) {
