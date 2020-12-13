0ba38a3d6fe1d251fd6bdd15e68cbbf8f2106360
John Roberts <john.calvin.roberts@gmail.com>
2020-12-13 18:27:31 +0800

    completes day10 part two challenge
    Failure, I did not come up with this solution

--- src/day10/part_two.ts
+++ src/day10/part_two.ts
@@ -1,8 +1,65 @@
 import path from 'path';
-import { readFile } from '@utils';
+import { readFile } from '../utils';
+
+// const countBranches = (
+//   adapters: number[],
+//   index: number,
+//   max: number,
+//   memo: Map<number, number> = new Map<number, number>(),
+// ): number => {
+//   const current = adapters[index];
+//   if (current === max) {
+//     return 1;
+//   }
+//   let result = 0;
+//   for (let i = 0; i <= 3; i++) {
+//     const next = adapters[index + i];
+//     if (next - current <= 3) {
+//       if (memo.has(index + i)) {
+//         result += memo.get(index + i) || 0;
+//       } else {
+//         const branchResult = countBranches(adapters, index + i, max, memo);
+//         memo.set(index + i, branchResult);
+//         result += branchResult;
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+const countBranches = (
+  jolts: number[],
+  index: number,
+  builtIn: number,
+  cache: Map<number, number> = new Map<number, number>(),
+): number => {
+  const current = jolts[index];
+  if (current === builtIn) {
+    return 1;
+  }
+  let result = 0;
+  for (let i = 1; i <= 3; i++) {
+    const next = jolts[index + i];
+    if (next - current <= 3) {
+      if (cache.has(index + i)) {
+        result += cache.get(index + i) || 0;
+      } else {
+        const branchResult = countBranches(jolts, index + i, builtIn, cache);
+        cache.set(index + i, branchResult);
+        result += branchResult;
+      }
+    }
+  }
+  return result;
+};
 
 export const runner = (input: string): number => {
-  return 0;
+  const adapters = input
+    .split('\n')
+    .map((item) => parseInt(item))
+    .sort((a, b) => a - b);
+  const max = adapters[adapters.length - 1];
+  return countBranches([0, ...adapters], 0, max);
 };
 
 if (require.main === module) {
--- src/day10/tests/day10.test.ts
+++ src/day10/tests/day10.test.ts
@@ -5,9 +5,13 @@ import path from 'path';
 
 describe('--- Day 10: Adapter Array ---', () => {
   let example: string;
+  let simpleExample: string;
   let input: string;
   beforeAll(async () => {
     example = await readFile(path.resolve(__dirname, '../example.txt'));
+    simpleExample = await readFile(
+      path.resolve(__dirname, '../simple_example.txt'),
+    );
     input = await readFile(path.resolve(__dirname, '../input.txt'));
   });
 
@@ -24,8 +28,19 @@ describe('--- Day 10: Adapter Array ---', () => {
   });
 
   describe('part two', () => {
-    it('should work with the example', () => {});
+    it('should work with the simple example', () => {
+      const res = partTwo(simpleExample);
+      expect(res).toEqual(8);
+    });
 
-    it('should work with the input', () => {});
+    it('should work with the example', () => {
+      const res = partTwo(example);
+      expect(res).toEqual(19208);
+    });
+
+    it('should work with the input', () => {
+      const res = partTwo(input);
+      expect(res).toEqual(99214346656768);
+    });
   });
 });
